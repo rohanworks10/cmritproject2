421d1fafc619cd26940fba7b4ea25cb88770f2b1
RohanGull <rohan.g.work10@gmail.com>
2026-05-22 17:01:52 +0530

    add reviews, blog, hashtag filter fixed

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { Navigation } from '@/components/navigation'
 import BlogListing from '@/components/blog/blog-listing'
 
@@ -6,7 +7,9 @@ export default function BlogIndex() {
     <div className="min-h-screen pb-28">
       <Navigation />
       <main className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-        <BlogListing />
+        <Suspense fallback={<div>Loading...</div>}>
+          <BlogListing />
+        </Suspense>
       </main>
     </div>
   )
